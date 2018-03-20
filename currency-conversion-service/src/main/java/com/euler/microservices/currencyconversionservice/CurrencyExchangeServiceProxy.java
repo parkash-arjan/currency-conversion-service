23a7a84a2f789ae84dcc6eedff1b9e09a3c2f320
parkash-arjan <parkash.arjan.github@gmail.com>
2018-03-19 23:05:55 -0400

    added description for path variables

--- currency-conversion-service/src/main/java/com/euler/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/euler/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CurrencyExchangeServiceProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to) ;
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to) ;
 	
 }
